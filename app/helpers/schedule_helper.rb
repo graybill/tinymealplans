a64a517f520f30acb52877d5a23c94674ba93512
Linus Graybill <graybill@gmail.com>
2012-10-18 12:06:29 -0400

    Add food icons generated from food.name regex

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -7,4 +7,18 @@ module ScheduleHelper
     return true if (DateTime.now + 1.day).strftime("%Y-%m-%d") == meal.date.strftime("%Y-%m-%d")
   end
 
+  def food_icon(food)
+    url = ""
+    icons = Dir.glob(Rails.root + "public/food-icons/*.gif")
+    icons.each do |icon|
+      icon_name = File.basename(icon).split(".")[0]
+      # match icon_name to food.name
+      r = Regexp.new(food.name.underscore.singularize)
+      if icon_name.underscore =~ r || food.name.underscore =~ Regexp.new(icon_name.underscore)
+        url = "/food-icons/#{icon_name}.gif"
+      end
+    end
+    url
+  end
+
 end
--- test/unit/helpers/schedule_helper_test.rb
+++ test/unit/helpers/schedule_helper_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class ScheduleHelperTest < ActionView::TestCase
+
+  test "returns food icon" do
+    assert_equal "images/food-icons/ChineseCabbage.gif", food_icon(FactoryGirl.create(:food, :name => "ChineseCabbage"))
+    assert_equal "images/food-icons/ChineseCabbage.gif", food_icon(FactoryGirl.create(:food, :name => "ChineseCabbages"))
+    assert_equal "images/food-icons/Carrot.gif", food_icon(FactoryGirl.create(:food, :name => "carrot"))
+  end
+
 end
