750045676ab6f457717f0ffc2b653c577e47575a
Linus Graybill <graybill@gmail.com>
2012-10-18 23:07:51 -0400

    Expand random name arrays

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,11 +2,11 @@ module UsersHelper
 
 
   def random_name
-    @adjectives = ["dirty", "dry", "arid", "awkward", "ambitious", "benevolent", "controlling", "effective", "faulty", "gigantic", "holistic"]
-    @names = ["duck", "bobby", "sue", "billy", "diane", "milfred", "wilfred", "xander", "steven", "derek", "cindy", "sandra", "joseph", "nicole", "debbie", "juan"]
+    @adjectives = ["dirty", "dry", "arid", "awkward", "ambitious", "benevolent", "controlling", "effective", "faulty", "gigantic", "holistic", "frustrated", "negative", "positive", "prolific"]
+    @names = ["duck", "bobby", "sue", "billy", "diane", "milfred", "wilfred", "xander", "steven", "derek", "cindy", "sandra", "joseph", "nicole", "debbie", "juan", "ryan", "matt", "mike", "cecilia", "karen", "nancy", "megan", "mak", "john"]
     name = "#{@adjectives.sample}-#{@names.sample}"
     if User.find_by_handle(name)
-      name = "#{names}-#{rand(1..1200)}"
+      name = "#{name}-#{rand(1..1200)}"
     end
     name
   end
